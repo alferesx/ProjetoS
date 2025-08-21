9ac615517961d94faba367466387c803a363a0ae
Su <alferesx@gmail.com>
2025-08-21 07:58:50 -0300

    Pulo implementado
    criado a função de pulo para o Player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,15 +1,15 @@
+using Unity.Burst.Intrinsics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public float speed = 2f;
-
-    public float jumpForce = 2f;
+    public float jumpForce = 8f;
     public Vector2 playerDirection;
-    float playerHorizontalDirection;
-    float playerVerticalDirection;
-
+    float moveX;
     private Rigidbody2D rb;
+
+    bool jumpRequested;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,26 +17,25 @@ public class Player : MonoBehaviour
     }
     void Update()
     {
-        playerHorizontalDirection = Input.GetAxis("Horizontal");
-        playerVerticalDirection = Input.GetAxis("Vertical");
-        playerDirection = new Vector2(playerHorizontalDirection, 0);
-
-       
+        moveX = Input.GetAxisRaw("Horizontal");
+        playerDirection = new Vector2(moveX, 0);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpRequested = true;    
+        }
     }
+
     void FixedUpdate()
     {
-        rb.linearVelocity = playerDirection * speed ;
-         if (Input.GetKeyDown(KeyCode.Space))
+        rb.linearVelocity = new Vector2(moveX * speed, rb.linearVelocity.y);
+        if (jumpRequested)
         {
-            Jump();
-            Debug.Log("Pula");
-        }
-        
-    }
-        void Jump()
-        {
-            Vector2 v = rb.linearVelocity;
-            v.y = jumpForce;
-            rb.linearVelocity = v;
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+            jumpRequested = false;   
         }
     }
+
+
+
+
+}
